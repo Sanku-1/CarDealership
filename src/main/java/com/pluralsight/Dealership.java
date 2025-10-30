347b26fabdb27c679ae4a71c1406c3e0b2e444f3
Stephen_A <ankustephen99@gmail.com>
2025-10-30 06:30:42 -0400

    Added getters and setters to Vehicle and Dealership classes

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -8,9 +8,34 @@ public class Dealership {
     private String phone;
     private ArrayList<Vehicle> inventory;
 
-    public Dealership(String name, String address, String phone) {
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> inventory) {
         this.name = name;
         this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 }
