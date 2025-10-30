608f804b518ae84701a404459b0678231eb21b2a
Stephen_A <ankustephen99@gmail.com>
2025-10-30 06:19:13 -0400

    Assigned attributes and created constructor for Dealership class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 }
+
